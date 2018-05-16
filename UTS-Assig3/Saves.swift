5ab765af4d342de95356ea1b20ce7ac3aaf79b76
Ian Zhou <12971572@student.uts.edu.au>
2018-05-16 16:27:44 +1000

    added Save.swift file closes #7

---
+++ UTS-Assig3/Saves.swift
@@ -0,0 +1,14 @@
+//
+//  Saves.swift
+//  UTS-Assig3
+//
+//  Created by Ian Zhou on 16/5/18.
+//  Copyright © 2018 Asher. All rights reserved.
+//
+
+import Foundation
+
+// For saving user answers on questions
+class Saves {
+    
+}
